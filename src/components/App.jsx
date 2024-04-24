417b7f7af761aa9ff2c87c2604e8243856d1a47c
Monika-Lis <lis.monikak@gmail.com>
2024-04-24 23:27:07 +0100

    loader2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,6 @@ const API_KEY = '42646310-ef56125427efcfe7b949942a4';
 export const App = () => {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [loadMoreError, setLoadMoreError] = useState('');
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const [loadMore, setLoadMore] = useState(false);
@@ -26,15 +25,13 @@ export const App = () => {
         const response = await axios.get(
           `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
         );
+
         setImages(prevImages => [...prevImages, ...response.data.hits]);
-        setLoadMore(response.data.total > page * 12);
-        setLoadMoreError('');
+        setLoadMore(response.data.totalHits > page * 12);
       } catch (error) {
-        Notiflix.Notify.info('Error fetching data');
+        Notiflix.Notify.error('Error fetching data');
       } finally {
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 1000);
+        setIsLoading(false);
       }
     };
     fetchImages();
@@ -47,25 +44,24 @@ export const App = () => {
     setLoadMore(false);
   };
 
-  const onLoadMore = async () => {
+  const onLoadMore = async event => {
+    event.preventDefault();
     if (!loadMore) {
-      loadMoreError();
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
       );
       return;
     }
+
     setPage(prevPage => prevPage + 1);
   };
 
   return (
     <div className={css.div}>
       <Searchbar onSearch={onSearch}></Searchbar>
-      {isLoading ? (
-        <Loader></Loader>
-      ) : (
-        <ImageGallery images={images}></ImageGallery>
-      )}
+
+      <ImageGallery images={images} />
+      {isLoading && <Loader />}
       {images.length > 0 && <Button onLoadMore={onLoadMore}>Load More</Button>}
     </div>
   );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import css from './Button.module.css';
 export const Button = ({ onLoadMore, children }) => {
   return (
     <div>
-      <button className={css.button} onClick={onLoadMore}>
+      <button type="button" className={css.button} onClick={onLoadMore}>
         {children}
       </button>
     </div>
